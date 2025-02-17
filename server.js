e22c8e4cf702b920acdd5f31320e3c5eaebda81d
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-02-17 14:27:41 +0000

    Initial dependencies import and server setup

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const expressLayouts = require ('express-ejs-layouts')
+
+// Setting our view engine (EJS) and layout
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+
+// Setting up the location of our static files
+app.use(express.static('public'))
+
+//Setting up server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log('Server is listening on port: ' + PORT);
+})
