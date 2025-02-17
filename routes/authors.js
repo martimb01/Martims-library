9d3d16d21cfcfca54e34b335769379c865a8f208
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-02-17 17:41:21 +0000

    Fixing mongoDB connection and creating author routes

---
+++ routes/authors.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+
+//GET all authors
+
+router.get('/', (req,res) => {
+    res.render('authors/index')
+})
+
+// GET new author
+
+router.get('/new', (req,res) => {
+    res.render('authors/new')
+})
+
+// Create new author
+router.post('/', (req,res) => {
+    res.send('Creating a new author')
+})
