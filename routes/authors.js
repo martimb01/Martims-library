be418b7d9bdb5d5bcb177b24c52b12122cf4c0af
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-02-18 16:34:16 +0000

    Created author module and creating form to submit a new author

--- routes/authors.js
+++ routes/authors.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const Author = require('../models/author'); 
 
 
 //GET all authors
-
 router.get('/', (req,res) => {
     res.render('authors/index')
 })
 
 // GET new author
-
 router.get('/new', (req,res) => {
-    res.render('authors/new')
+    res.render('authors/new', { author: new Author() })
 })
 
 // Create new author
