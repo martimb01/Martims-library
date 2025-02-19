fa4caf6ebad1806cc8c3b034e1f12c0922244283
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-02-19 21:35:05 +0000

    Added recently added books to my homepage, filter by date still doesnt work, dont know why

--- routes/books.js
+++ routes/books.js
@@ -24,10 +24,10 @@ router.get('/', async (req,res) => {
         query = query.regex('title', new RegExp(req.query.title, 'i'))
     }
     if (req.query.publishedBefore != null && req.query.publishedBefore != '') {
-        query = query.lte('publishedDate', new Date(req.query.publishedBefore))
+        query = query.lte('publishedDate', req.query.publishedBefore)
     }
     if (req.query.publishedAfter != null && req.query.publishedAfter != '') {
-        query = query.gte('publishedDate', new Date(req.query.publishedAfter))
+        query = query.gte('publishedDate', req.query.publishedAfter)
     }
     try{
         const books = await query.exec()
