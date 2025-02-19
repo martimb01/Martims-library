039f5afc42cc865b80bdd90a3e03f10b57c1c682
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-02-19 21:51:31 +0000

    Fixed the date filteringgit add .git add .

--- routes/books.js
+++ routes/books.js
@@ -19,17 +19,18 @@ const upload = multer({
 //GET all books
 router.get('/', async (req,res) => {
     let query = Book.find()
-    console.log(req.query.publishedBefore)
+    console.log('Query params', req.query)
     if (req.query.title != null && req.query.title != '') {
         query = query.regex('title', new RegExp(req.query.title, 'i'))
     }
     if (req.query.publishedBefore != null && req.query.publishedBefore != '') {
-        query = query.lte('publishedDate', req.query.publishedBefore)
+        query = query.lte('publishDate', req.query.publishedBefore)
     }
     if (req.query.publishedAfter != null && req.query.publishedAfter != '') {
-        query = query.gte('publishedDate', req.query.publishedAfter)
+        query = query.gte('publishDate', req.query.publishedAfter)
     }
     try{
+        console.log('Constructed Query:', query);
         const books = await query.exec()
         res.render('books/index', {
             books: books,
