67e0bc145cfc1e964ebbcfcd80bf3c10eb4987b8
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-02-21 17:39:55 +0000

    Finally fixed deleted method with proper constrains

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Book = require('./book')
 
 const authorSchema = new mongoose.Schema({
     name: {
@@ -7,4 +8,18 @@ const authorSchema = new mongoose.Schema({
     }
 })
 
+authorSchema.pre("deleteOne", async function (next) {
+    try {
+        const query = this.getFilter();
+        const hasBook = await Book.exists({ author: query._id });
+  
+        if (hasBook) {
+            next(new Error("This author still has books."));
+        } else {
+            next();
+        }
+    } catch (err) {
+        next(err);
+    }
+});
 module.exports = mongoose.model('Author', authorSchema)
--- routes/authors.js
+++ routes/authors.js
@@ -37,7 +37,7 @@ router.post('/', async (req,res) => {
     })
     try {
         const newAuthor = await author.save();
-        res.redirect('authors')
+        res.redirect(`authors/${newAuthor.id}`)
 
     } catch {
         res.render('authors/new', {
@@ -65,13 +65,40 @@ router.get('/:id/edit', async (req,res) => {
     
 })
 
-router.put('/:id', (req,res) => {
-    res.send('Update author' + req.params.id)
+router.put('/:id', async (req,res) => {
+    let author
+    try {
+        author = await Author.findById(req.params.id)
+        author.name = req.body.name
+        await author.save()
+        res.redirect(`/authors/${author.id}`)
+
+    } catch {
+        if (author == null) {
+            res.redirect('/')
+        } else {
+            res.render('authors/edit', {
+                author: author,
+                errorMessage: 'Error updating the Author!'
+            })
+        }
+    }
 })
 
-router.delete('/:id', (req,res) => {
-    res.send('Delete author' + req.params.id)
-})
+router.delete('/:id', async (req, res) => {
+    let author;
+    try {
+        author = await Author.findById(req.params.id);
+        await author.deleteOne();
+        res.redirect('/authors');
+    } catch {
+        if (author == null) {
+            res.redirect('/');
+        } else {
+            res.redirect(`/authors/${author.id}`);
+        }
+    }
+});
 
 
 
