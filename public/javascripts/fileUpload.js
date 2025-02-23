6fc1572d7bae0ece50f7851c6dbc27365224738f
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-02-23 18:31:34 +0000

    Finished styling

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,3 +1,30 @@
+const rootStyles = window.getComputedStyle(document.documentElement)
+
+if (rootStyles.getPropertyValue('--book-cover-width-large') != null && 
+    rootStyles.getPropertyValue('--book-cover-width-large') != '') {
+    ready()
+} else {
+    document.getElementById('main-css').addEventListener('load', ready)
+}
+
+function ready() {
+    const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width-large'))
+    const coverRatio = parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-ratio'))
+    const coverHeight = coverWidth / coverRatio
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize,
+        FilePondPluginFileEncode,
+    )
+    
+    FilePond.setOptions({
+        stylePanelAspectRatio: 1 / coverRatio,
+        imageResizeTargetWidth: coverWidth,
+        imageResizeTargetHeight: coverHeight
+    })
+    FilePond.parse(document.body)
+}
+
 FilePond.registerPlugin(
     FilePondPluginImagePreview,
     FilePondPluginImageResize,
